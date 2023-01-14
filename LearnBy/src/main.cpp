36868e35908f58efbd73cf2fc0de1fb04390e502
CarfYunxl <carf_yunxl@163.com>
2023-01-14 22:22:56 +0800

    add LearnBy

---
+++ LearnBy/src/main.cpp
@@ -0,0 +1,26 @@
+#include "handle_memory.h"
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: server <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_context io_context;
+
+        using namespace std; // For atoi.
+        server s(io_context, atoi(argv[1]));
+
+        io_context.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
